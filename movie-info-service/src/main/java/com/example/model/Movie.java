5899961984537e27f0cbe721ad482368c5247ad6
javama9 <mokhjav19@gmail.com>
2020-09-07 21:31:36 -0500

    Added more properites

--- movie-info-service/src/main/java/com/example/model/Movie.java
+++ movie-info-service/src/main/java/com/example/model/Movie.java
@@ -19,6 +19,7 @@ public class Movie {
 	private String name;
 	private Date releaseDate;
 	private String description;
+	private String details;
 	
 	public Long getId() {
 		return id;
@@ -31,6 +32,12 @@ public class Movie {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
 	}
 	public Date getReleaseDate() {
 		return releaseDate;
